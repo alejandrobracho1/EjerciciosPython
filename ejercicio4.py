9d4ed96c6ea44b26d565c450e6098d9ba3b44c8c
Enrique <alejandrobacho1@gmail.com>
2023-02-27 07:24:44 -0400

    .

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,3 +1,11 @@
-print("Esto es una prueba de el ejercicio 4")
-
-print("Esto es un mensaje de prueba 2 ")
+# Declaracion de Variables
+suma = 0.0
+numero = 0
+ 
+# Solicitud de Datos
+print("Introduce un número entero: ")
+numero = int(input("> "))
+ 
+# Cálculo de la suma
+suma = numero * (numero + 1) / 2
+print("La suma desde 1 hasta " + str(numero) + " es " + str(suma))
---
+++ ejercicio5.py
@@ -0,0 +1,13 @@
+#Declaracion de variables 
+area = 0.0
+lado = 0
+
+
+#Solicitud de datos
+print("Introduzca la medida del lado del cuadrado: ")
+lado = int(input(">"))
+
+area = lado ** 2
+
+#Salida 
+print("El area del cuadrado es: " + str(area))
